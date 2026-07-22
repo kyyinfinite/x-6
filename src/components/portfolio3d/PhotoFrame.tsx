7908665caf9f5c09a2a26cef7073a7c75427fd17
kyyinfinite <kyyinfinite@gmail.com>
2026-07-22 18:48:08 +0700

    benrin frame

--- src/components/portfolio3d/PhotoFrame.tsx
+++ src/components/portfolio3d/PhotoFrame.tsx
@@ -78,20 +78,21 @@ function PhotoFrameInner({ project, position, rotationY, active }: PhotoFramePro
         <meshBasicMaterial color="#000000" transparent opacity={0.16} depthWrite={false} />
       </mesh>
 
-      {/* Aksen tepi luar (gaya bingkai kayu/gold saat aktif) */}
-      <mesh position={[0, 0, -0.021]} frustumCulled={false}>
-        <boxGeometry args={[matW + 0.05, matH + 0.05, 0.018]} />
+      {/* Aksen tepi luar (gaya bingkai kayu/gold saat aktif) -> paling belakang */}
+      <mesh position={[0, 0, -0.03]} frustumCulled={false}>
+        <boxGeometry args={[matW + 0.05, matH + 0.05, 0.02]} />
         <meshStandardMaterial color={active ? "#A6772C" : "#2B2620"} roughness={0.5} metalness={0.15} />
       </mesh>
 
-      {/* Mat/kartu polaroid krem */}
-      <mesh position={[0, 0, -0.01]} frustumCulled={false}>
+      {/* Mat/kartu polaroid krem -> di depan aksen, permukaan depan ada di z=+0.02 */}
+      <mesh position={[0, 0, 0]} frustumCulled={false}>
         <boxGeometry args={[matW, matH, 0.04]} />
         <meshStandardMaterial color={active ? "#FFFDF7" : "#F5F0E4"} roughness={0.7} metalness={0.02} envMapIntensity={0.4} />
       </mesh>
 
-      {/* Foto asli -> ukuran mengikuti aspect ratio, tidak pernah gepeng */}
-      <mesh position={[0, photoOffsetY, 0.005]} frustumCulled={false}>
+      {/* Foto asli -> WAJIB diletakkan di depan permukaan mat (z lebih besar dari 0.02),
+          kalau tidak foto akan "terkubur" di dalam box mat dan terlihat seolah tidak ter-load. */}
+      <mesh position={[0, photoOffsetY, 0.021]} frustumCulled={false}>
         <planeGeometry args={[photoW, photoH]} />
         <meshBasicMaterial map={texture} toneMapped={false} />
       </mesh>
@@ -103,11 +104,11 @@ function FallbackFrame({ position, rotationY, project }: Pick<PhotoFrameProps, "
   const tilt = useMemo(() => hashTilt(project.id), [project.id]);
   return (
     <group position={position} rotation={[0, rotationY, tilt]} frustumCulled={false}>
-      <mesh position={[0, 0, -0.01]} frustumCulled={false}>
+      <mesh position={[0, 0, 0]} frustumCulled={false}>
         <boxGeometry args={[1.78, 2.08, 0.04]} />
         <meshStandardMaterial color="#F5F0E4" roughness={0.7} />
       </mesh>
-      <mesh position={[0, 0.1, 0.005]} frustumCulled={false}>
+      <mesh position={[0, 0.1, 0.021]} frustumCulled={false}>
         <planeGeometry args={[1.5, 1.5]} />
         <meshStandardMaterial color="#c9bda3" roughness={0.9} />
       </mesh>
